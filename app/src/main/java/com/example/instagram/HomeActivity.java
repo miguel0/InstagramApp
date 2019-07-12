40487356705c35e56d8d8850276a6bcdfae8fe65
miguel0 <miguelbarroe@gmail.com>
2019-07-11 17:59:48 -0700

    final commit

--- app/src/main/java/com/example/instagram/HomeActivity.java
+++ app/src/main/java/com/example/instagram/HomeActivity.java
@@ -11,7 +11,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-
 import com.example.instagram.fragments.ComposeFragment;
 import com.example.instagram.fragments.HomeFragment;
 import com.example.instagram.fragments.UserFragment;
